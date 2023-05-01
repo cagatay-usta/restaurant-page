e1393af4c9c4b60ac14cfacce9ee0caa238291dc
cagatay-usta <cagatay_usta@windowslive.com>
2023-05-01 14:44:21 +0300

    initialize src files

---
+++ src/homePage.js
@@ -0,0 +1,8 @@
+function component() {
+    const element = document.createElement("div");
+    element.innerHTML = "hello world!!!";
+    element.classList.add("hello");
+    return element;
+  }
+
+  export default component
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-import bgimage from "./kopernikbg.jpeg";
+import "./style.css";
+import component from "./homePage";
 
-function component() {
-  const element = document.createElement("div");
 
-  element.innerHTML = "hello world!!!";
+document.getElementById("content").appendChild(component());
 
-  const image = new Image();
-  image.src = bgimage;
-  image.height = 300;
-
-  element.appendChild(image);
-
-  return element;
-}
-
-document.body.appendChild(component());
